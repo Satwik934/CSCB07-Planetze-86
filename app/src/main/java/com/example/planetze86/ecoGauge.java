216a7f1a057a5ab0b2c4c96776921028fb9e11a5
AkshatPatel2 <akshatpatel1303@gmail.com>
2024-11-21 13:38:48 -0600

    feat: Created Eco Gauge and Updated MPAndroidChart

---
+++ app/src/main/java/com/example/planetze86/ecoGauge.java
@@ -0,0 +1,23 @@
+package com.example.planetze86;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.planetze86.R;
+
+public class ecoGauge extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eco_gauge);
+
+    }
+}
