191b3ebe50a7a6af314225e55752a4a811328e1e
AkshatPatel2 <akshatpatel1303@gmail.com>
2024-11-22 10:39:12 -0600

    feat: Added constants to maintain consistent naming conventions.

---
+++ app/src/main/java/com/example/planetze86/utils/FirebaseConstants.java
@@ -0,0 +1,10 @@
+package com.example.planetze86.utils;
+
+public class FirebaseConstants {
+    // Constants to ensure proper naming convention throughout Firebase field names
+    public static final String TRANSPORTATION = "transportation";
+    public static final String FOOD = "food";
+    public static final String HOUSING = "housing";
+    public static final String CONSUMPTION = "consumption";
+    public static final String USER_EMISSIONS = "userEmissions";
+}
