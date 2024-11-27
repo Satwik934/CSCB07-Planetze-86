7c7f44148ac87d3dfe92b42bdaf5752a8e432119
AkshatPatel2 <akshatpatel1303@gmail.com>
2024-11-27 01:06:40 -0600

    feat: Added a button for accessing Planetze Menu

--- app/src/main/java/com/example/planetze86/ecotracker.java
+++ app/src/main/java/com/example/planetze86/ecotracker.java
@@ -1,7 +1,9 @@
 package com.example.planetze86;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,11 +12,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ecotracker extends AppCompatActivity {
 
+    Button menuButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ecotracker);
 
+
+        // Redirects the button to open eco gauge.
+        menuButton = findViewById(R.id.planetze_menu_button);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ecotracker.this, PlanetzeMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
